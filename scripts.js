6adc63b4575548a53b27823be9692c0142f41f81
solybarr <solybarr@gmail.com>
2020-04-28 19:00:51 -0500

    Email Validation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 function checkEmail() {
     var theEmail = document.getElementById("email").value;
-    if ((theEmail.trim() === "") || (theEmail.trim() === "abc") ) {
+
+    var matchTest = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(theEmail));
+
+    if ((theEmail.trim() === "") || !matchTest ) {
         msg = "Please provide a valid email";
+        document.getElementById("email").style.backgroundImage = "url('images/icon-error.svg')";
+        document.getElementById("email").style.backgroundRepeat = "no-repeat";
+        document.getElementById("email").style.backgroundPosition = "83%";
+        document.getElementById("email").style.border = "0.1em solid #f96262";      
     }
     else {
+        document.getElementById("email").style.background= "none";
         msg="Thank you";
     }
     //alert(msg);
     document.getElementById("error").innerHTML = msg;
+    return;
 }
